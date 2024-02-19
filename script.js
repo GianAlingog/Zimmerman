83df162523f087c3109c622b050c6ca51cde5e5f
GianAlingog <76099610+GianAlingog@users.noreply.github.com>
2024-02-18 21:44:51 -0500

    Final Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  const image = Math.floor(Math.random() * 7) + 1;
+  gif.src = `images/${image}.jpg`;
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+
+  const image = Math.floor(Math.random() * 7) + 1;
+  gif.src = `images/${image}.jpg`;
+});
